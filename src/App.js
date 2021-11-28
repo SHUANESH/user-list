5b053656acadc576c25069ff0152de070868074c
SHUANESH <shuanesh1817@gmail.com>
2021-11-28 18:11:17 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+
+  const [users, setUsers] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+const clickHandler = async ()=>{
+  await fetch("https://gorest.co.in/public/v1/users")
+   .then(res => res.json())
+   .then(res => setUsers(res.data))
+   .catch((error)=> console.error("Error!! "+ error))
+
+   setIsOpen(true);
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" title="app"> 
+
+    <h1>Get User List</h1>
+       <button title="get-user-button" onClick={clickHandler}>click me</button>
+
+       {
+         isOpen? users?.map((user , index)=>{
+           return <div title="app-user-container" key={index} className="app-user-card">
+                <h2 title="app-h2-user" style={{color: user.status === "active"? "green": "red"}}>{user.name}</h2>  
+                <h5>{user.status}</h5>
+           </div>
+
+         }) : ""
+       }
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("onClickEvent", ()=>{
+  it("onclick", ()=>{
+    const {queryByTitle} = render(<App/>)
+    const getUser = queryByTitle("get-user-button");
+    const userInfo = queryByTitle("app-h2-user");
+    fireEvent.click(getUser);
+    console.log(userInfo);
+    expect(userInfo).toBe(`<h2 title="app-h2-user" style={{color: user.status === "active"? "green": "red"}}>{user.name}</h2><h5>{user.status}</h5>`)
+  })
+
+  it("Before on click", ()=>{
+    const {queryByTitle} = render(<App/>)
+    const userInfo = queryByTitle("app");
+    expect(userInfo).toBeInTheDocument()
+  })
+
+})
+
+describe("onClickEvent", ()=>{
+
+})
